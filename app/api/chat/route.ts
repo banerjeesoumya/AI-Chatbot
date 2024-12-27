8081e1bc1021b7f0a2808e4b17e8aa23d2e25f2b
soumyarikbanerjee <126083869+banerjeesoumya@users.noreply.github.com>
2024-12-28 01:22:39 +0530

    Added system interactions and changed the dimensions

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -13,6 +13,9 @@ export async function POST(req: Request) {
 
     const result = streamText({
         model: google("gemini-1.5-pro-latest"),
+        system: `You are a helpful assistant. Check your knowledge base before answering any questions.
+        Only respond to questions using information from tool calls.
+        if no relevant information is found in the tool calls, respond, "Sorry, I don't know."`,
         messages: convertToCoreMessages(messages)
     });
 
